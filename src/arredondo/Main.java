8df098c589ecf881497a642ac77343f5438d0844
Nefi <amkamisaki@hotmail.com>
2021-05-14 19:05:11 -0300

    Code added

--- src/arredondo/Main.java
+++ src/arredondo/Main.java
@@ -3,6 +3,8 @@ package arredondo;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello my name is Carlos");
+
+        System.out.println("Hello my name is Carlos");
+        System.out.println("Hello, Carlos. My name is Armando.");
     }
 }
